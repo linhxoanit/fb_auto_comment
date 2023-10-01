b256ae331c2d0d1a2e0d2e62e28c8456686d0095
Linh Nguyen <linhxoanit@gmail.com>
2023-10-01 15:13:34 +0700

    update app id

--- script/login.js
+++ script/login.js
@@ -1,16 +1,13 @@
-const appSecret = '13ae4d829eba9f8d771a8875ca0385bc';
-const appId = '3593112904254172';
+const appSecret = 'b628d2387c6059306d1e7a1fe460b7e5';
+const appId = '1069626954415267';
 window.fbAsyncInit = function() {
     FB.init({
-        appId      : '3593112904254172',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v18.0'
+      appId      : '1069626954415267',
+      xfbml      : true,
+      version    : 'v18.0'
     });
-
     FB.AppEvents.logPageView();
-
-};
+  };
 
 function checkLoginState() {
     FB.getLoginStatus(function(response) {
@@ -19,9 +16,9 @@ function checkLoginState() {
     });
 }
 (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
